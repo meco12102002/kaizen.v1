2430cb9e34cc8dc5137dac40ef043a17651552c1
mecs <128163435+meco12102002@users.noreply.github.com>
2026-06-25 22:31:48 +0800

    started from scratch

--- lib/app/theme/app_theme.dart
+++
@@ -1,7 +0,0 @@
-import 'light_theme.dart';
-import 'dark_theme.dart';
-
-class AppTheme {
-  static final light = LightTheme.theme;
-  static final dark = DarkTheme.theme;
-}
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:kaizen/app/theme/app_theme.dart';
-import 'package:kaizen/features/tasks/presentation/pages/onboarding_controller.dart';
+
 
 void main() {
   runApp(const MyApp());
@@ -13,10 +12,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Onboarding(),
-      theme: AppTheme.light,
-      darkTheme: AppTheme.dark,
-      themeMode: ThemeMode.system,
+      home: Placeholder()
     );
   }
 }
